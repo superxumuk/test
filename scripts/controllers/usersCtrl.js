024c905cf158c11e61646fd0e7343bc72203e493
superxumuk <supermegaxumuk@mail.ru>
2016-01-30 08:14:57 +0300

    delete

--- scripts/controllers/usersCtrl.js
+++ scripts/controllers/usersCtrl.js
@@ -6,11 +6,15 @@
         { id: 4, fname: 'Евгений', lname: 'Пашков', comt: '' },
 		{ id: 5, fname: 'Светлана', lname: 'Моисеева', comt: 'gfngfng' }		
     ]; 
-
+	
 	$scope.create = function() { 
 		$scope.newClient.id = $scope.clients.length + 1; 
 		$scope.clients.push($scope.newClient); 
 		$scope.newClient = null; 
 	};
-
+	
+	$scope.delete = function(client) { 
+		var index = $scope.clients.indexOf(client); 
+		$scope.clients.splice(index, 1); 
+	};
 });
