09afe5cd36c4e8581c8effce7eb167c989fc4161
superxumuk <supermegaxumuk@mail.ru>
2016-01-30 07:55:54 +0300

    v1.0

---
+++ scripts/controllers/usersCtrl.js
@@ -0,0 +1,9 @@
+﻿angular.module('clientsApp').controller('clientsCtrl', function($scope) { 
+    $scope.clients = [ 
+        { id: 1, fname: 'Илья', lname: 'Иванов', comt: 'fghfgh' }, 
+        { id: 2, fname: 'Виталий', lname: 'Петров', comt: 'hbjkhjk' }, 
+        { id: 3, fname: 'Александр', lname: 'Сидоров', comt: 'rttry' }, 
+        { id: 4, fname: 'Евгений', lname: 'Пашков', comt: '' },
+		{ id: 5, fname: 'Светлана', lname: 'Моисеева', comt: 'gfngfng' }		
+    ]; 
+});
